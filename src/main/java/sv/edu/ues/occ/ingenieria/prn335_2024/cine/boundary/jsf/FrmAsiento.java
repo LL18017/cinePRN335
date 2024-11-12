146e8dbafa09ee550b837b9d53b05815f4406e07
mjlop99 <mario992lopez@gmail.com>
2024-11-12 08:52:57 -0600

    se agrego frm asiento

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/FrmAsiento.java
@@ -0,0 +1,54 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.boundary.jsf;
+
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import org.primefaces.event.SelectEvent;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.control.AbstractDataPersist;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Asiento;
+
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+@Named
+@ViewScoped
+public class FrmAsiento extends AbstractFrm<Asiento> implements Serializable {
+    @Override
+    public void instanciarRegistro() {
+        registro = new Asiento();
+    }
+
+    @Override
+    public FacesContext getFC() {
+        return null;
+    }
+
+    @Override
+    public AbstractDataPersist<Asiento> getAbstractDataPersist() {
+        return null;
+    }
+
+    @Override
+    public String getIdByObject(Asiento object) {
+        if (object != null) {
+            return object.getIdAsiento().toString();
+        }
+        return "";
+    }
+
+    @Override
+    public Asiento getObjectById(String id) {
+        if (id!=null && modelo!=null && modelo.getWrappedData()!=null) {
+            return modelo.getWrappedData().stream().filter(a->id.equals(a.getIdAsiento().toString())).findFirst().orElse(null);
+        }
+        return null;
+    }
+
+    @Override
+    public void selecionarFila(SelectEvent<Asiento> event) {
+
+    }
+
+    @Override
+    public String paginaNombre() {
+        return "Asientos";
+    }
+}
---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/control/AsientoBean.java
@@ -0,0 +1,23 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.control;
+
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Asiento;
+
+import java.io.Serializable;
+@Stateless
+@LocalBean
+public class AsientoBean extends AbstractDataPersist<Asiento> implements Serializable {
+    @PersistenceContext(unitName = "cinePU")
+    EntityManager em;
+    public AsientoBean() {
+        super(Asiento.class);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+}
