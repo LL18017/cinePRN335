f4c5ecb308ab881b471cd3d595a89706b64e5f68
LL18017 <ll18017@ues.edu.sv>
2024-12-05 21:32:20 -0600

    reportes

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/rest/resouce/ReporteResource.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/rest/resouce/ReporteResource.java
@@ -7,10 +7,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.StreamingOutput;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-
+import net.sf.jasperreports.engine.*;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,48 +22,64 @@ public class ReporteResource implements Serializable {
     @Resource(lookup = "jdbc/pgdb")
     DataSource pool;
     @GET
-    @Path("{reporte}")
+    @Path("{reporte}/{idReserva}")
     public Response getReporte(
-            @PathParam("reporte")
-            String reporte
+            @PathParam("reporte") String reporte,
+            @PathParam("idReserva") Integer idReserva
     ) {
-        HashMap parametros= new HashMap();
-        String path;
+        HashMap<String, Object> parametros = new HashMap<>();
+        String path = null;
         switch (reporte) {
             case "tiposala":
-//                path = getClass().getResource("/reportes/tipoSalaRPT.jasper").toString();
                 path = "/reportes/TipoSalaRPT.jasper";
                 break;
-
+            case "salas":
+                path = "/reportes/Sala.jasper";
+                parametros.put("SUBREPORT_DIR", getClass().getResource("/reportes/").getPath());
+                break;
+            case "salacaracteristica":
+                path = "/reportes/SalaCaracteristicas.jasper";
+                break;
+            case "ticket":
+                path = "/reportes/ticketRPT.jasper";
+                if (idReserva == null) {
+                    return Response.status(Response.Status.BAD_REQUEST)
+                            .header("Missing-Parameter", "idReserva")
+                            .build();
+                }
+                parametros.put("idReserva", idReserva); // Agregar parámetro para el reporte
+                break;
             default:
                 return Response.status(Response.Status.NOT_FOUND)
                         .header("Report-NotFound", reporte)
                         .build();
         }
-        if (path!=null){
+        if (path != null) {
             try {
+                InputStream fuenteDeReporte = getClass().getResourceAsStream(path);
 
-            InputStream fuenteDeReporte = getClass().getResourceAsStream(path);
-
-            if (fuenteDeReporte!=null){
-               JasperPrint impresor= JasperFillManager.fillReport(fuenteDeReporte,parametros,pool.getConnection());
-
-                StreamingOutput salida=new StreamingOutput() {
-                    @Override
-                    public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-                        try {
-                            JasperExportManager.exportReportToPdfStream(impresor,outputStream);
-                        }catch (Exception e){
-                            Logger.getLogger(ReporteResource.class.getName()).log(Level.SEVERE, null, e);
-                            throw new IOException("NO SE PUDO EXPORTAR EL REPORTE");
+                if (fuenteDeReporte != null) {
+                    JasperPrint impresor = JasperFillManager.fillReport(fuenteDeReporte, parametros, pool.getConnection());
+                    StreamingOutput salida = new StreamingOutput() {
+                        @Override
+                        public void write(OutputStream outputStream) throws IOException, WebApplicationException {
+                            try {
+                                JasperExportManager.exportReportToPdfStream(impresor, outputStream);
+                            } catch (Exception e) {
+                                Logger.getLogger(ReporteResource.class.getName()).log(Level.SEVERE, null, e);
+                                throw new IOException("NO SE PUDO EXPORTAR EL REPORTE");
+                            }
                         }
-                    }
-                };
-
-                return Response.ok(salida,"application/pdf").build();
-            }
-            }catch (Exception e){
-                Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+                    };
+                    return Response.ok(salida, "application/pdf").build();
+                } else {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Resource not found at: " + path);
+                    return Response.status(Response.Status.NOT_FOUND)
+                            .header("Resource-NotFound", path)
+                            .build();
+                }
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
             }
         }
         return Response.serverError().build();
